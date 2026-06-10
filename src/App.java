ae1206c0422147f571ef18c76053c23ed0f16aa6
santiagosatama393 <santiagosatama393@gmail.com>
2026-06-10 07:24:17 -0500

    frist

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int[] arreglo = {12, -7, 25, 0, -15, 33, 19, -22, 5, 48, -3};
         int opcion = 0;
 
@@ -12,7 +12,7 @@ public class App {
             System.out.println("1. Ejecutar ordenamientos");
             System.out.println("2. Salir");
             System.out.print("Ingrese una opcion: ");
-            
+
             opcion = sc.nextInt();
 
             if (opcion == 1) {
@@ -23,16 +23,16 @@ public class App {
                 boolean ascShell = sc.nextBoolean();
 
                 Insertion insercion = new Insertion();
-                Shell shell = new Shell();
+                shell metodoShell = new shell();
 
-                
                 insercion.sort(arreglo, ascInsercion);
-                shell.sort(arreglo, ascShell);
+                metodoShell.sort(arreglo, ascShell);
 
             } else if (opcion == 2) {
                 System.out.println("Saliendo...");
             }
         } while (opcion != 2);
+
         sc.close();
     }
 }
--- src/shell.java
+++ src/shell.java
@@ -1,58 +1,67 @@
-public class Shell {
+public class shell {
+
     public void sort(int[] array, boolean asc) {
         int[] copia = array.clone();
         int comparaciones = 0, cambios = 0, iteraciones = 0;
 
         System.out.println("\n===== METODO SHELL =====");
         System.out.print("Arreglo original: ");
-        for (int v : copia) System.out.print(v + " ");
+        for (int v : copia) {
+            System.out.print(v + " ");
+        }
         System.out.println();
 
         int gap = copia.length / 2;
+
         while (gap > 0) {
             for (int i = gap; i < copia.length; i++) {
                 iteraciones++;
+
+                int temp = copia[i];
+                int j = i;
+
                 System.out.print("\nI" + iteraciones + "  ");
-                for (int v : copia) System.out.print(v + "  ");
+                for (int v : copia) {
+                    System.out.print(v + "  ");
+                }
                 System.out.println();
 
-                int j = i;
                 while (j >= gap) {
                     comparaciones++;
-                    int ant = j - gap;
-                    boolean condicion = asc ? (copia[ant] > copia[j]) : (copia[ant] < copia[j]);
 
-                    
-                    System.out.print("      gap=" + gap + " a=" + ant + " b=" + j + " [a]=" + copia[ant] + " [b]=" + copia[j]);
+                    boolean condicion = asc 
+                            ? copia[j - gap] > temp 
+                            : copia[j - gap] < temp;
+
+                    System.out.print("      gap=" + gap 
+                            + " a=" + (j - gap) 
+                            + " b=" + j 
+                            + " [a]=" + copia[j - gap] 
+                            + " [b]=" + temp);
 
                     if (condicion) {
                         System.out.println("  cambio=si");
-                        int valorA = copia[ant];
-                        int valorB = copia[j];
 
-                        int aux = copia[j];
-                        copia[j] = copia[ant];
-                        copia[ant] = aux;
-                        cambios++;
-
-                        
-                        System.out.print("      ");
-                        for (int k = 0; k < ant; k++) System.out.print("   ");
-                        System.out.println(valorB);
-
-                        System.out.print("      ");
-                        for (int k = 0; k < j; k++) System.out.print("   ");
-                        System.out.println(valorA);
-
+                        copia[j] = copia[j - gap];
                         j -= gap;
+                        cambios++;
                     } else {
                         System.out.println("  cambio=no");
                         break;
                     }
                 }
+
+                copia[j] = temp;
             }
+
             gap /= 2;
         }
+
+        System.out.print("\nArreglo ordenado: ");
+        for (int v : copia) {
+            System.out.print(v + " ");
+        }
+
         System.out.println("\nCOMPARACIONES = " + comparaciones);
         System.out.println("ITERACIONES   = " + iteraciones);
         System.out.println("CAMBIOS       = " + cambios);
